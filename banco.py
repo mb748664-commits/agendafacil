816ae2ca77110ee7a3a7c6b8fd47df7889d93a17
Mariana Barbosa Silva <mb748664@gmail.com>
2026-09-09 01:18:05 -0300

    Adiciona porta do banco online

--- banco.py
+++ banco.py
@@ -5,6 +5,7 @@ import os
 def conectar_banco():
     conexao = mysql.connector.connect(
         host=os.getenv("DB_HOST", "localhost"),
+        port=int(os.getenv("DB_PORT", "3306")),
         user=os.getenv("DB_USER", "root"),
         password=os.getenv("DB_PASSWORD", ""),
         database=os.getenv("DB_NAME", "plataforma_agendamento")
